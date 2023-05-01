774396b2e48b254b9d59f415590450cea69ea63d
tezalve <tezalve@gmail.com>
2023-05-02 00:29:50 +0600

    :tada: setup project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAv8ZKY9UJ32JQaSE_S7a9WjqYSkfX1Z6g",
+  authDomain: "delish-15656.firebaseapp.com",
+  projectId: "delish-15656",
+  storageBucket: "delish-15656.appspot.com",
+  messagingSenderId: "649975777824",
+  appId: "1:649975777824:web:6274392d410130d7870a45"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
